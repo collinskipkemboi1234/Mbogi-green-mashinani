30ef557b613fde0dadf9cd9b9b82efa3ddac9637
collinskipkemboi1234 <collinskipkemboi1234@gmail.com>
2025-10-29 07:31:42 +0300

    Add files via upload

---
+++ src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_globals_b805903d.css",
+  "static/chunks/node_modules_66d5deb9._.js",
+  "static/chunks/src_ef1b125e._.js"
+],
+    source: "dynamic"
+});
